d6f0b2e5d8e25d320ecea3a445bce207c0c1b5cc
alexzx396 <arcealex257@gmail.com>
2023-11-06 17:54:47 -0300

    Actualizacion de divicion de secciones

---
+++ javascriptfile.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+    function loadContent(id, filePath) {
+      fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById(id).innerHTML = data;
+          if (id === 'contacto-placeholder') {
+            AOS.init();
+          }
+        })
+        .catch(error => console.error(`Error loading ${id}:`, error));
+    }
+
+    loadContent('footer-placeholder', 'footer.html');
+    loadContent('contacto-placeholder', 'contacto.html');
+    loadContent('skills-placeholder', 'skills.html');
+    loadContent('phrase-placeholder', 'phrase.html');
+    loadContent('project-placeholder', 'project.html');
+    loadContent('generalProjects-placeholder', 'generalProjects.html');
+    loadContent('aboutMe-placeholder', 'aboutMe.html');
+    
+  });
+  
