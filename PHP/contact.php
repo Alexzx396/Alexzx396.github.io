1beda53c34679cb3e24f0c37b5438cb0e2da8c89
alexzx396 <arcealex257@gmail.com>
2023-11-07 00:00:52 -0300

    niner

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,28 +1,34 @@
 <?php
+// Comprobamos si el formulario ha sido enviado usando el método POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $mensaje = $_POST['mensaje'];
+    // Recogemos los datos del formulario
+    $nombre = trim($_POST['nombre']);
+    $email = trim($_POST['email']);
+    $mensaje = trim($_POST['mensaje']);
 
-    // La dirección a la que quieres enviar el mensaje
+    // La dirección de correo electrónico a la que quieres enviar el mensaje
     $destinatario = 'arcealex257@gmail.com';
-    $asunto = 'Mensaje desde mi WEB personal';
-    
+    $asunto = 'Mensaje desde mi sitio web';
+
     // Preparando el cuerpo del mensaje
     $cuerpo = "Nombre: " . $nombre . "\n";
     $cuerpo .= "Email: " . $email . "\n";
     $cuerpo .= "Mensaje: " . $mensaje . "\n";
 
     // Cabeceras adicionales
-    $cabeceras = "From: no-reply@yourdomain.com" . "\r\n";
+    $cabeceras = "https://alexzx396.github.io" . "\r\n"; 
     $cabeceras .= "Reply-To: " . $email . "\r\n";
     $cabeceras .= "X-Mailer: PHP/" . phpversion();
 
     // Enviar el email
-    mail($destinatario, $asunto, $cuerpo, $cabeceras);
+    $envio = mail($destinatario, $asunto, $cuerpo, $cabeceras);
 
-    // Redireccionar a una página de gracias
-    header('Location: gracias.html');
-    exit;
+    // Comprobamos si el correo fue enviado con éxito y redirigimos
+    if ($envio) {
+        header('Location: gracias.html');
+        exit;
+    } else {
+        echo "Lo sentimos, ocurrió un error al enviar tu mensaje.";
+    }
 }
 ?>
