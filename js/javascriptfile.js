1129f10ada52de1565445aa8f35a2c1f3b0b4a40
alexzx396 <arcealex257@gmail.com>
2023-11-06 21:28:18 -0300

    actualizacion

---
+++ js/javascriptfile.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Función para manejar la respuesta del fetch.
+  function handleResponse(id, data) {
+    document.getElementById(id).innerHTML = data;
+    if (id === "contacto-placeholder") {
+      AOS.init();
+    }
+  }
+
+  // Función para manejar los errores.
+  function handleError(id, error) {
+    console.error(`Error loading ${id}:`, error);
+  }
+
+  // Función para cargar el contenido.
+  function loadContent(id, filePath) {
+    fetch(filePath)
+      .then((response) => response.text())
+      .then((data) => handleResponse(id, data))
+      .catch((error) => handleError(id, error));
+  }
+
+  // Carga todo el contenido en paralelo.
+  const placeholders = [
+    { id: "footer-placeholder", filePath: "footer.html" },
+    { id: "contacto-placeholder", filePath: "contacto.html" },
+    { id: "skills-placeholder", filePath: "skills.html" },
+    { id: "phrase-placeholder", filePath: "phrase.html" },
+    { id: "project-placeholder", filePath: "project.html" },
+    { id: "generalProjects-placeholder", filePath: "generalProjects.html" },
+    { id: "aboutMe-placeholder", filePath: "aboutMe.html" },
+  ];
+
+  // Inicia todas las solicitudes de fetch simultáneamente.
+  placeholders.forEach((placeholder) =>
+    loadContent(placeholder.id, placeholder.filePath)
+  );
+});
